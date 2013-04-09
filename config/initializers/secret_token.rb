32c151964b67b9d97608b121a2f6ece479c21ef9
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-04-09 18:05:18 -0400

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WF::Application.config.secret_token = '4bb1cc6f92cc779e5d4c45aa553d50f00dcdcd0799c30b9991ec0ccc7bdff3e47a3bddc20a7e9cb73f3f0bf2ec3b1e1fb3c976030ae7fa34df9cf327037413b2'
