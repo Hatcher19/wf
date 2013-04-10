0abee5c3dd24fff1e4f5233b2680c4c6f5a5cc7b
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-04-10 11:10:23 -0400

    add user model with devise

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 group :production do
 	gem 'pg'
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module WF
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #require for devise on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
