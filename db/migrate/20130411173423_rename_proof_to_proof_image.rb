03e2e46401c054eada1478ca81ef97c6d81fb845
Daniel Hatcher <Daniel.Jon.Hatcher@gmail.com>
2013-04-12 11:32:50 -0400

    delete proof

--- db/migrate/20130411173423_rename_proof_to_proof_image.rb
+++
@@ -1,8 +0,0 @@
-class RenameProofToProofImage < ActiveRecord::Migration
-  def change
-    rename_column :projects, :proof_file_name, :image_proof_file_name
-		rename_column :projects, :proof_content_type, :image_proof_content_type
-		rename_column :projects, :proof_file_size, :image_proof_file_size
-		rename_column :projects, :proof_updated_at, :image_proof_updated_at
-	end
-end
